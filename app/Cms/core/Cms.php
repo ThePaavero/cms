cd19243a1e2e441522725732502a84eac5c0357c
Pekka <nospam@astudios.org>
2015-12-21 21:19:06 +0200

    Ding

--- app/Cms/core/Cms.php
+++ app/Cms/core/Cms.php
@@ -3,12 +3,16 @@
 namespace App\Cms\Core;
 
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\View;
 
 class Cms
 {
     public function __construct()
     {
         $this->config = include __DIR__ . '/../Config/cms.php';
+
+        $viewDir = __DIR__ . '/../Templates';
+        View::addNamespace('cms', $viewDir);
     }
 
     public function processUri($uri)
@@ -20,6 +24,15 @@ class Cms
             App::abort(404);
         }
 
-        dd($mappedPage);
+        $templateSlug = $mappedPage->templateSlug;
+
+        $template = $template = new Template($templateSlug);
+
+        if ( ! $template->slugMapsToFile())
+        {
+            App::abort(500, 'Template file missing (' . $templateSlug . ').');
+        }
+
+        return view('cms::' . $templateSlug, []);
     }
 }
--- app/Cms/core/Template.php
+++ app/Cms/core/Template.php
@@ -4,8 +4,16 @@ namespace App\Cms\Core;
 
 class Template
 {
-    public function __construct()
+    public function __construct($slug)
     {
-        // ...
+        $this->slug = $slug;
+    }
+
+    public function slugMapsToFile()
+    {
+        $fileName = $this->slug . '.blade.php';
+        $templateFilePath = __DIR__ . '/../Templates/' . $fileName;
+
+        return file_exists($templateFilePath);
     }
 }
