47bc47bd323b2444bcbdf362d5ad1906647626cf
Pekka <nospam@astudios.org>
2015-12-22 21:34:35 +0200

    FUCK!

--- app/Cms/Core/Sitemap.php
+++ app/Cms/Core/Sitemap.php
@@ -13,49 +13,6 @@ class Sitemap
     {
         $pages = $this->pages->toArray();
 
-        $tree = [];
-
-        foreach ($pages as $page)
-        {
-            $segments = explode('/', $page['uri']);
-
-            if ( ! isset($page['children']))
-            {
-                $page['children'] = [];
-            }
-
-            if ( ! isset($tree[$segments[0]]))
-            {
-                $tree[$segments[0]] = [];
-            }
-
-            $current = &$tree;
-            for ($i = 0; $i < count($segments); $i ++)
-            {
-                $segment = $segments[$i];
-                if ( ! isset($current[$segment]))
-                {
-                    $current[$segment] = [];
-                }
-
-                if ($i === count($segments) - 1)
-                {
-                    array_push($current[$segment], $page);
-                }
-
-                $current = &$current[$segment];
-            }
-        }
-
-        $tree = $this->cleanUpNestedArray($tree);
-
-        return $tree;
+        dd('aaargh');
     }
-
-    public function cleanUpNestedArray($tree)
-    {
-        // @todo :)
-        return $tree;
-    }
-
 }
